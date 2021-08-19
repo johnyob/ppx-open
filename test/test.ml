14e56f1a9f8d6df390387baf802ca13c66a306dc
johnyob <ajo41@cam.ac.uk>
2021-08-19 12:24:09 +0100

    Fix tests.

--- test/test.ml
+++ test/test.ml
@@ -1,8 +1,34 @@
-(** test1 *)
-[%%open Module_foo.(val1)]
+module Foo = struct
+  type 'a t = Foo of 'a
 
-(** test2 *)
-[%%open Module_foo.(val1, val2)]
+  let val1 = Foo 1
+  let val2 = Foo 2
 
-(** test3 *)
-[%%open Module_foo.(val1, val2 [@as renamed1])]
+  module type S = sig
+    type 'a t
+
+    val val1 : int t
+    val val2 : int t
+  end
+
+  module Bar = struct
+    type t =
+      | A
+      | B
+      | C
+
+    let to_string = function
+      | A -> "A"
+      | B -> "B"
+      | C -> "C"
+  end
+end
+
+[%%open {| Foo.(val1) |}]
+[%%open {| Foo.(val1, val2 as renamed1) |}]
+[%%open {| Foo.(module Bar, module Bar as B) |}]
+[%%open {| Foo.(module type S, module type S as Foo_S) |}]
+
+(* [%%open {| Foo.(type t as foo_t) |} ]
+
+[%%open {| Foo.(type t (..) as foo_t) |} ] *)
